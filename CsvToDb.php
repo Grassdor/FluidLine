fd154b1483554a0f100e2cc8852989389f27be96
Grassdor <rrr345@mail.ru>
2023-11-21 16:35:17 +0300

    correcting script for import

--- CsvToDb.php
+++ CsvToDb.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App;
 
 require_once("/Develop/FluidLine/Config/Config.php");
@@ -9,54 +10,81 @@ use Config\Config;
 
 class CsvToDb
 {
-    public function exportCsv($file): string
+    private PDO $pdo;
+
+    private array $updatedValues = [];
+
+    public function __construct()
     {
-        while ($lineArr = fgetcsv($file, null, ";")) {
-            try {
-                $this->updateDb($lineArr);
-            } catch (PDOException $e) {
-                echo "Ошибка записи в БД: " . $e->getMessage() . PHP_EOL;
-            }
-
-        }
-        return "rew";
-        echo $lineArr;
-    }
-
-    private function updateDb(array $data): void //updates data in you DB
-    {
-
         $db = new Config();
         $dbConn = $db->dbConn();
 
-        $pdo = new PDO($dbConn["dsn"], $dbConn["username"], $dbConn["password"]);
+        $this->pdo = new PDO($dbConn["dsn"], $dbConn["username"], $dbConn["password"]);
+    }
+
+    public function exportCsv($file): void
+    {
+        $rowPosition = 0;
+        
+        while ($lineArr = fgetcsv($file, separator: ";")) {
+            if ($rowPosition) {
+                try {
+                    $this->updateDb($this->pdo, $lineArr);
+                } catch (PDOException $e) {
+                    echo "Ошибка записи в БД: " . $e->getMessage() . PHP_EOL;
+                }
+            }
+            
+            $rowPosition++;
+        }
+
+
+    }
+
+    private function updateDb(PDO $pdo, array $data): void //updates data in you DB
+    {
         $preparedData = $this->dataEncoder($data);
         $contentid = $preparedData[0];
         $pagetitle = $preparedData[1];
         $tmplvarid = $preparedData[2];
         $value = $preparedData[3];
-        
-        $sql = $pdo->prepare("UPDATE product_tmplvar_data SET `value` = :value
-                              WHERE id = (
-                                SELECT `value` FROM product_tmplvar_contentvalues
-                                WHERE tmplvarid = :tmplvarid
-                                AND contentid = :contentid
-                              )");
 
+        $getId = $pdo->prepare("
+            SELECT `value` FROM `product_tmplvar_contentvalues`
+            WHERE `tmplvarid` = :tmplvarid AND `contentid` = :contentid
+        ");
         
-        $sql->bindParam(':contentid', $contentid);
-        $sql->bindParam(':tmplvarid', $tmplvarid);
-        $sql->bindParam(':value', $value);
-        $sql->execute();
-        echo $preparedData[3] . PHP_EOL;
+        $getId->bindParam('contentid', $contentid);
+        $getId->bindParam('tmplvarid', $tmplvarid);
+        $getId->execute();
+
+        $response = $getId->fetch(PDO::FETCH_ASSOC);
+
+        if (isset($response['value'])) {
+            if (!in_array((int) $response['value'], $this->updatedValues)) {
+                $sql = $pdo->prepare("
+                    UPDATE `product_tmplvar_data` AS ptd SET ptd.`value` = :valuen
+                    WHERE `id` = :idn;
+                ");
+    
+                $sql->bindParam('valuen', $value);
+                $sql->bindParam('idn', $response['value']);
+            
+                $sql->execute();
+
+                $this->updatedValues[] = $response['value'];
+            }
+        }
     }
 
     private function dataEncoder(array $data): array //encodes data from Excel encode to UTF-8
     {
         $result = [];
+
         foreach ($data as $item) {
-            $result[] = iconv("Windows-1251", "UTF-8", $item);
+            $result[] = mb_convert_encoding($item, "UTF-8", "Windows-1251");
         }
+
         return $result;
     }
 }
