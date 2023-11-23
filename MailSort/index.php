ea40ba137a925e7398f0dd6fdcd963bd00b3e3e5
Grassdor <rrr345@mail.ru>
2023-11-23 10:27:38 +0300

    add method for get all unify mail domen, change write file from txt to csv

--- MailSort/index.php
+++ MailSort/index.php
@@ -1,32 +1,41 @@
 <?php
 ini_set('memory_limit', '256M');
+error_reporting(E_ALL);
 
 require_once('./src/MailProcessor.php');
 
 use App\MailSort\MailProcessor;
 
-$dictionaryProcessor = new MailProcessor();
+$mailProcessor = new MailProcessor();
+$message = "";
 
 if (php_sapi_name() === 'cli') {
     echo "Введите URL с данными для обработки: ";
     $fileUrl = trim(fgets(STDIN));
-    $dictionaryProcessor->processFile($fileUrl);
+    $mailProcessor->processFile($fileUrl);
 } else {
-    ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Сортировка почтовых файлов</title>
-    </head>
-    <body>
+    if (!empty($_FILES)) {
+        $mailProcessor->handleWebUpload($_FILES['userFile']['tmp_name']);
+        $message = "Сортировка доменов прошла успешно, ваши файлы находятся в папке library";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Сортировка почтовых доменов</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+
+<body>
+    <p><?= $message ?></p>
     <form method="post" enctype="multipart/form-data">
         <input name="userFile" type="file"><br>
         <button type="submit">Начать обработку</button>
     </form>
-    </body>
-    </html>
-    <?php
-    $dictionaryProcessor->handleWebUpload($_FILES['userFile']['tmp_name']);
-}
+</body>
+
+</html>
--- MailSort/src/MailProcessor.php
+++ MailSort/src/MailProcessor.php
@@ -3,6 +3,9 @@ namespace App\MailSort;
 
 class MailProcessor
 {
+    private array $unifyDomens = [];
+    private array $unifyMails = [];
+
     public function processFile($url)
     {
         $content = file_get_contents($url);
@@ -10,37 +13,66 @@ class MailProcessor
 
         $words = preg_split("/\r\n|\r/", $content);
         foreach ($words as $word) {
-            $this->processWord($word);
+            
+            $domen = $this->processWord($word);
+            if (!in_array($domen, $this->unifyDomens)) {
+                $this->unifyDomens[] = $domen;
+                $this->unifyMails[] = $word;
+
+            }
         }
+        $this->unifyMailList($this->unifyMails);
     }
 
-    private function processWord($word)
+    private function processWord(string $word)
     {
         if (!empty($word)) {
             $wordLetters = preg_split('/[@]/', $word);
             $folderName = implode("_", preg_split('/\./', $wordLetters[1]));
             $folderPath = "library/{$folderName}";
-            $filePath = "library/{$folderName}/words.txt";
             if (!file_exists($folderPath)) {
                 mkdir($folderPath, 0777, true);
             }
-            file_put_contents($filePath, $word . PHP_EOL, FILE_APPEND);
+            $filePath = fopen("library/{$folderName}/domens.csv", "a+");
+            
+            fputcsv($filePath, [iconv("UTF-8", "Windows-1251", $word)]);
+            fclose($filePath);
+            
+
         } else {
             echo "No match found for word: $word\n";
         }
+
+        return $wordLetters[1];
     }
 
     public function handleWebUpload($file)
     {
         if (! empty($file)) {
             $content = file_get_contents($file);
-            $content = mb_convert_encoding($content, 'UTF-8', mb_detect_encoding($content));
-
             $words = preg_split("/\r\n|\r/", $content);
 
             foreach ($words as $word) {
-                $this->processWord($word);
+                $domen = $this->processWord($word);
+                if (!in_array($domen, $this->unifyDomens)) {
+                    $this->unifyDomens[] = $domen;
+                    $this->unifyMails[] = $word;
+                }
             }
+            $this->unifyMailList($this->unifyMails);
+        }
+    }
+
+    private function unifyMailList(array $list): void
+    {
+        foreach ($list as $item) {
+            $folderPath = "library/!unify";
+            if (!file_exists($folderPath)) {
+                mkdir($folderPath, 0777, true);
+            }
+            $filePath = fopen("library/!unify/domens.csv", "a+");
+            fputcsv($filePath, [iconv("UTF-8", "Windows-1251", $item)]);
+            fclose($filePath);
         }
     }
 }
